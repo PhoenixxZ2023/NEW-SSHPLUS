248a082d8f5dbd662389d39c1468531a8e6b8441
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2025-06-22 00:02:03 -0300

    Update config.py

--- Modulos/xray_util/util_core/config.py
+++ Modulos/xray_util/util_core/config.py
@@ -2,18 +2,22 @@
 # -*- coding: utf-8 -*-
 import pkg_resources
 import configparser
-from v2ray_util import run_type
+# MODIFICAÇÃO 1: Corrigido o nome do pacote importado
+from xray_util import run_type
 
-CONF_FILE = '/etc/v2ray_util/util.cfg'
-DATA_FILE = 'util.dat' if run_type == "v2ray" else 'xray.dat'
+# MODIFICAÇÃO 2: Atualizado o caminho do arquivo de configuração
+CONF_FILE = '/etc/xray_util/util.cfg'
+# MODIFICAÇÃO 3: Lógica simplificada, pois o run_type será sempre 'xray'
+DATA_FILE = 'xray.dat'
 
 class Config:
 
     def __init__(self):
         self.config = configparser.ConfigParser()
         self.config_path = CONF_FILE
-        self.data_path = pkg_resources.resource_filename('v2ray_util', DATA_FILE)
-        self.json_path = pkg_resources.resource_filename('v2ray_util', "json_template")
+        # MODIFICAÇÃO 4: Atualizadas as referências internas ao nome do pacote
+        self.data_path = pkg_resources.resource_filename('xray_util', DATA_FILE)
+        self.json_path = pkg_resources.resource_filename('xray_util', "json_template")
         self.config.read(self.config_path)
 
     def get_path(self, key):

db45d910922fc1dafc646380312017d43652abab
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2025-06-22 15:37:52 -0300

    Update ban_bt.py

--- Modulos/xray_util/global_setting/ban_bt.py
+++ Modulos/xray_util/global_setting/ban_bt.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from ..util_core.loader import Loader
-from ..util_core.v2ray import restart
+# MODIFICAÇÃO ÚNICA: Corrigido o nome do arquivo importado de 'v2ray' para 'xray'.
+from ..util_core.xray import restart
 from ..util_core.writer import GlobalWriter
 from ..util_core.utils import readchar
 

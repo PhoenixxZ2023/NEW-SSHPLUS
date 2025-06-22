1c0bfe29bdde831f0313148f2bb9f1d7d546a78c
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2025-06-22 18:04:34 -0300

    Update setup.py

--- Modulos/setup.py
+++ Modulos/setup.py
@@ -4,28 +4,22 @@ from setuptools import setup, find_packages
 
 import xray_util
 
-try:
-    with open("README.md", "r", encoding='UTF-8') as fh:
-        long_description = fh.read()
-except FileNotFoundError:
-    long_description = "Painel Xray personalizado por PhoenixxZ2023"
+with open("README.md", "r", encoding='UTF-8') as fh:
+    long_description = fh.read()
 
 setup(
     name='xray-util',
     version=xray_util.__version__,
-    description="uma ferramenta para gerenciar a configuração do xray",
+    description="a tool to manage xray config json",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    keywords='python xray vless trojan xtls reality',
-    author='PhoenixxZ2023',
-    author_email='seu-email@exemplo.com',
+    keywords='python v2ray multi-v2ray vmess socks5 vless trojan xray xtls reality',
+    author='Jrohy',
+    author_email='euvkzx@gmail.com',
     url='https://github.com/PhoenixxZ2023/NEW-SSHPLUS',
     license='GPL',
     packages=find_packages(),
-    
-    # Esta linha diz ao setup para procurar e usar o arquivo MANIFEST.in
     include_package_data=True,
-    
     zip_safe=False,
     python_requires='>=3',
     entry_points={
@@ -40,5 +34,13 @@ setup(
         "Natural Language :: English",
         'Natural Language :: Chinese (Simplified)',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ]
 )

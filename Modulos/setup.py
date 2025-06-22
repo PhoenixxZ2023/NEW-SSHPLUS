cc1e46b5af781561a65ba71a02ebe20d3c9210f8
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2025-06-22 17:19:33 -0300

    Update setup.py

--- Modulos/setup.py
+++ Modulos/setup.py
@@ -5,22 +5,26 @@ from setuptools import setup, find_packages
 # Importa do novo nome da pasta
 import xray_util
 
-with open("README.md", "r", encoding='UTF-8') as fh:
-    long_description = fh.read()
+# MODIFICAÇÃO 1: Removida a tentativa de abrir o README.md para evitar o erro.
+# with open("README.md", "r", encoding='UTF-8') as fh:
+#     long_description = fh.read()
 
 setup(
     # Nome do pacote para o pip
     name='xray-util',
     # Pega a versão a partir do novo nome
     version=xray_util.__version__,
-    description="uma ferramenta para gerenciar a configuração do xray", # Descrição atualizada
-    long_description=long_description,
+    description="uma ferramenta para gerenciar a configuração do xray",
+    
+    # MODIFICAÇÃO 2: Adicionada uma descrição simples no lugar do conteúdo do README.
+    long_description="Painel Xray personalizado por PhoenixxZ2023",
+    
     long_description_content_type="text/markdown",
-    keywords='python xray vless trojan xtls reality', # Keywords atualizadas
-    # Seus dados de autor (opcional)
-    author='PhoenixxZ2023', # Seu nome
+    keywords='python xray vless trojan xtls reality',
+    # Seus dados de autor
+    author='PhoenixxZ2023',
     author_email='seu-email@exemplo.com',
-    url='https://github.com/PhoenixxZ2023/NEW-SSHPLUS', # URL do seu repositório
+    url='https://github.com/PhoenixxZ2023/NEW-SSHPLUS',
     license='GPL',
     packages=find_packages(),
     include_package_data=True,
@@ -28,7 +32,7 @@ setup(
     python_requires='>=3',
     entry_points={
         'console_scripts': [
-            # Cria o comando 'xray-util' que aponta para a função 'menu' no 'main.py' dentro da nova pasta
+            # Cria o comando 'xray-util' que aponta para a função 'menu' no 'main.py'
             'xray-util = xray_util.main:menu'
         ]
     },

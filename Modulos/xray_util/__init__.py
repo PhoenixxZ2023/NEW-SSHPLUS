9818a58bda93249d68d79950ef2c489f4d9f0bb0
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2025-06-21 23:56:15 -0300

    Update __init__.py

--- Modulos/xray_util/__init__.py
+++ Modulos/xray_util/__init__.py
@@ -1,9 +1,8 @@
 __version__ = '3.11.4'
 
-import sys
-if "xray" in sys.argv[0]:
-    run_type = 'xray'
-else:
-    run_type = 'v2ray'
+# MODIFICAÇÃO: Definindo 'xray' como o tipo de execução padrão.
+# Removemos a lógica antiga que também suportava 'v2ray' para focar apenas em Xray.
+run_type = 'xray'
 
+# Importa a função de tradução, que é usada em outras partes do painel.
 from .util_core.trans import _

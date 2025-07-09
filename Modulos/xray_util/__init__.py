35006bd5a4ae67f4b163d0ee680b37600dd16390
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2025-07-08 21:49:18 -0300

    Delete Modulos/xray_util directory

--- Modulos/xray_util/__init__.py
+++
@@ -1,8 +0,0 @@
-__version__ = '3.11.4'
-
-# MODIFICAÇÃO: Definindo 'xray' como o tipo de execução padrão.
-# Removemos a lógica antiga que também suportava 'v2ray' para focar apenas em Xray.
-run_type = 'xray'
-
-# Importa a função de tradução, que é usada em outras partes do painel.
-from .util_core.trans import _
